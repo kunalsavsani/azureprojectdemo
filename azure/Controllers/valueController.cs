d0209cdc1f1f8d720f9a58bc305819a2abd9924a
849093 <849093@cognizant.com>
2020-07-16 15:49:29 +0530

    added

---
+++ azure/Controllers/valueController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace azure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class valueController : ControllerBase
+    {
+        // GET: api/value
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "azure", "project" };
+        }
+
+        // GET: api/value/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/value
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/value/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
